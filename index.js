1400d9d958e4bb3da625a0b9c4648a593dc5d691
pd2192013-prog <pd2192013@gmail.com>
2026-09-08 15:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ async function askGemini(question) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-goog-api-key": GEMINI_API_KEY,
+        "Authorization": `Bearer ${GEMINI_API_KEY}`,
       },
       body: JSON.stringify(body),
     });
