73ccd82a9b2ca0b64356ce6775eafb5310bf460b
pd2192013-prog <pd2192013@gmail.com>
2026-09-08 16:38:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 const BOT_TOKEN = "8961031495:AAEZncwlq5ZHKTDOwuO8rjRlGn1VkLDf-0g";
 const OPENROUTER_API_KEY = "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";
-const OPENROUTER_MODEL = "google/gemma-4-26b-a4b-it:free";
+const OPENROUTER_MODEL = "google/gemma-4-31b-it:free";
 
 const RATE_LIMIT_COUNT = 15;
 const RATE_LIMIT_WINDOW_MS = 60 * 60 * 1000; // 1 ghanta
